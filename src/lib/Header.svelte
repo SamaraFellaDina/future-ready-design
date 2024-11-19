32608afd85b87dc174e4f9f3c4bd5168289f39c5
Khdulkadir <khdulkadir@gmail.com>
2024-11-19 18:02:53 +0100

    popover test

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,32 +3,34 @@
   export let company;
 </script>
 
-<input type="checkbox" name="sidebar" id="sidebar" aria-controls="sidebar" aria-label="navigatiemenu"/>
-<label for="sidebar">
-  <Icon name = "sidebar" />
-</label>
 <header>
-  <a href ="/"><Icon name = "accessdash-logo" /></a>
-  <nav>
-    <h2><Icon name = "dashboard" /><span>Dashboard</span></h2>
-    <a class="active" href='/dashboard'>Dashboard</a>
-    <a href='/info'>Info</a>
-    <a href='/voortgang'>Voortgang</a>
-    <a href='/scan'>Scan</a>
-  </nav>
-  <nav>
-    <h2><Icon name = "page-links" /><span>{company[0].title}</span></h2>
-    <a class="active" href='/{company[0].title}/home'>/home</a>
-    <a href='/{company[0].title}/over-ons'>/over-ons</a>
-    <a href='/{company[0].title}/contact'>/contact</a>
-    <a href='/{company[0].title}/missie'>/missie</a>
-    <a href='/{company[0].title}/blog'>/blog</a>
-    <a href='/{company[0].title}/blog-detail'>/blog-detail</a>
-  </nav>
+  <div popover="auto" id="menu">
+    <button popovertarget="menu" aria-label="toggle navigatiemenu">
+      <Icon name = "sidebar" />
+      <span>Menu</span>
+    </button>
+    <a href ="/"><Icon name = "accessdash-logo" /></a>
+    <nav>
+      <h2><Icon name = "dashboard" /><span>Dashboard</span></h2>
+      <a class="active" href='/dashboard'>Dashboard</a>
+      <a href='/info'>Info</a>
+      <a href='/voortgang'>Voortgang</a>
+      <a href='/scan'>Scan</a>
+    </nav>
+    <nav>
+      <h2><Icon name = "page-links" /><span>{company[0].title}</span></h2>
+      <a class="active" href='/{company[0].title}/home'>/home</a>
+      <a href='/{company[0].title}/over-ons'>/over-ons</a>
+      <a href='/{company[0].title}/contact'>/contact</a>
+      <a href='/{company[0].title}/missie'>/missie</a>
+      <a href='/{company[0].title}/blog'>/blog</a>
+      <a href='/{company[0].title}/blog-detail'>/blog-detail</a>
+    </nav>
+  </div>
 </header>
 
 <style>
-  header {
+  div {
     position: fixed;
     background-color: var(--color-background-section);
     height: 100%;
@@ -41,6 +43,16 @@
     z-index: 1;
     width: 280px;
     overflow-y: auto;
+    border: 0;
+  }
+
+  div:popover-open {
+    transform: translateX(-80%);
+  }
+
+  div:popover-open > a {
+    opacity: 0;
+    pointer-events: none;
   }
 
   nav {
@@ -62,6 +74,10 @@
     color: var(--color-blue);
   }
 
+  button > span {
+    display: none;
+  }
+
   nav a {
     font-size: 1.15em;
     color: var(--color-regular-text);
@@ -77,30 +93,7 @@
     color: var(--color-blue);
   }
 
-  input {
-    position: absolute;
-    left: -100vw;
-  }
-
-  input:checked ~ header {
-    transform: translateX(-80%);
-  }
-
-  input:checked ~ label {
-    transform: translateX(-225px) scaleX(-1);
-  }
-
-  input:checked ~ header > a {
-    opacity: 0;
-    pointer-events: none;
-  }
-
-  input:focus ~ label {
-    border: 3px solid black;
-    border-radius: 5px;
-  }
-
-  label {
+  button {
     position: fixed;
     top: 20px;
     cursor: pointer;
@@ -109,52 +102,56 @@
     transition: 0.5s;
   }
 
-  header > a {
+  div:popover-open > button {
+    transform: scaleX(-1);
+  }
+
+  div > a {
     transition: 0.3s;
     margin-top: 30px;
   }
 
-  header > a:hover {
+   div > a:hover {
     opacity: 0.5;
   }
 
   @media screen and (max-width: 1700px) {
-    header {
+    div {
       transform: translateX(-80%);
       overflow-y: hidden;
     }
 
-    label {
-      transform: translateX(-225px) scaleX(-1);
-    }
-
-    input:checked ~ header {
+    div:popover-open {
       transform: translateX(0);
       overflow-y: auto;
     }
 
-    input:checked ~ label {
-      transform: translateX(0);
-    }
-
-    header > a {
+    div > a {
       opacity: 0;
       pointer-events: none;
     }
 
-    input:checked ~ header > a {
+    div:popover-open > a {
       opacity: 1;
       pointer-events: all;
+    }
+
+    div:popover-open > button {
+      transform: scaleX(1);
+    }
+
+    div > button {
+      transform: scaleX(-1);
     }
   }
 
   @media screen and (max-width: 700px) {
-    input:not(:checked) ~ header {
+    div:not(:popover-open) {
       background-color: transparent;
       box-shadow: none;
     }
 
-    input:not(:checked) ~ label {
+    div:not(:popover-open) > button {
       box-shadow: var(--box-shadow);
       background: #FFFFFF80;
       padding: 6px 8px;
