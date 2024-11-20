e58f4c33215acb84c5e4d14d6dcead822f241e22
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2024-11-20 10:35:08 +0100

    maken van #38 component
    voor #37 heb ik verschillende componenten ingedeeld in een library. Bij deze ook de Typegraph

---
+++ src/lib/3_Organism/TypeGraph.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { BarChart } from '../index.js'
+  export let result = data.scans;
+</script>
+
+<section>
+  <h2>Soorten foutmeldingen</h2>
+  <figure>
+    <BarChart {result}/>
+  </figure>
+</section>
+
+<style>
+  
+  section {
+    background-color: var(--color-background-section);
+    border-radius: var(--section-border-radius);
+    box-shadow: var(--box-shadow);
+    padding: var(--average-padding);
+    grid-area: 3 / 1 / 4 / 2;
+  }
+
+  h2 {
+    margin-bottom: 50px;
+  }
+</style>
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,14 +1,15 @@
 // place files you want to import through the `$lib` alias in this folder.
 export { default as Icon } from './1_Atoms/Icon.svelte'
+
+export { default as BarChart } from './2_Molecules/BarChart.svelte'
 export { default as DonutChart } from './2_Molecules/DonutChart.svelte'
 export { default as MonthlyNavigation } from './2_Molecules/MonthlyNavigation.svelte'
 
-
-export { default as Title } from './4_Templates/Title.svelte'
-export { default as AccessibillityGraph } from './4_Templates/AccessibillityGraph.svelte'
-export { default as AutomaticScan } from './4_Templates/AutomaticScan.svelte'
-export { default as ErrorResult } from './4_Templates/ErrorResult.svelte'
-export { default as TypeGraph } from './4_Templates/TypeGraph.svelte'
-export { default as Header } from './4_Templates/Header.svelte'
-export { default as Footer } from './4_Templates/Footer.svelte'
+export { default as Title } from './3_Organism/Title.svelte'
+export { default as AccessibillityGraph } from './3_Organism/AccessibillityGraph.svelte'
+export { default as AutomaticScan } from './3_Organism/AutomaticScan.svelte'
+export { default as ErrorResult } from './3_Organism/ErrorResult.svelte'
+export { default as TypeGraph } from './3_Organism/TypeGraph.svelte'
+export { default as Header } from './3_Organism/Header.svelte'
+export { default as Footer } from './3_Organism/Footer.svelte'
 
