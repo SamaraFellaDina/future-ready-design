52ffcca8882c7067de3d12561d195e2cebabde22
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2024-11-23 14:18:24 +0100

    #54 indelen van de library
    Hier heb ik de grafiek staan van de data en de stijlingen, zodat het in meerdere svelte bestanden kan worden gebruikt

---
+++ src/lib/1_Atoms/Chart.svelte
@@ -0,0 +1,43 @@
+<!-- Chart.svelte -->
+<script>
+  import { onMount } from 'svelte';
+  import Chart from 'chart.js/auto';
+
+  export let chartLabels = [];   
+  export let chartData = [];     
+  export let chartLabel = ''; 
+  export let chartBorderColor = '';
+  export let chartBackgroundColor = '';
+
+
+  let chart;
+
+  onMount(() => {
+    const ctx = document.getElementById('myChart').getContext('2d');
+    chart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: chartLabels,
+        datasets: [{
+          label: chartLabel,
+          data: chartData,
+          borderColor: chartBorderColor,
+          backgroundColor: chartBackgroundColor,
+          borderWidth: 3,
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+
+    return () => chart.destroy();
+  });
+</script>
+
+<canvas id="myChart"></canvas>
+
--- src/lib/2_Molecules/BarChart.svelte
+++ src/lib/2_Molecules/BarChart.svelte
@@ -1,174 +1,60 @@
 <script>
+  import {Chart} from '../index.js'
   export let result = data.scans;
 
-  import { onMount } from "svelte";
-  import Chart from "chart.js/auto";
-
-  let chart;
   let results = result.result.slice(1);
 
   let errorTitles = results.map((item) => item.title);
   let errorAmounts = results.map((item) => item.amount);
 
-  onMount(() => {
-    const rootStyles = getComputedStyle(document.documentElement);
-    const colorBlue = rootStyles.getPropertyValue("--color-blue");
-    const colorLightBlue = rootStyles.getPropertyValue("--color-lightblue");
+  const rootStyles = getComputedStyle(document.documentElement);
 
-    const fontFamily = rootStyles.getPropertyValue("--font-family");
-    const colorBlack = rootStyles.getPropertyValue("--color-black");
+  const colorStatusCheck = (data) => {
+    const colorStatusGood = rootStyles.getPropertyValue("--color-status-good") || 'green';
+    const colorStatusFine = rootStyles.getPropertyValue("--color-status-fine") || 'yellow';
+    const colorStatusBad = rootStyles.getPropertyValue("--color-status-bad") || 'red';
 
-    const colorStatusGood = rootStyles.getPropertyValue("--color-status-good");
-    const colorStatusGoodBorder = rootStyles.getPropertyValue("--color-status-good-border");
-
-    const colorStatusFine = rootStyles.getPropertyValue("--color-status-fine");
-    const colorStatusFineBorder = rootStyles.getPropertyValue("--color-status-fine-border");
-
-    const colorStatusBad = rootStyles.getPropertyValue("--color-status-bad");
-    const colorStatusBadBorder = rootStyles.getPropertyValue("--color-status-bad-border");
-
-
-    const colorStatusCheck = (data) => {
-      return data.map(value => {
-        if (value >=20) {
-          return colorStatusBad;
-        } else if (value >=10) {
-          return colorStatusFine;
-        } else {
-          return colorStatusGood
-        }
-      })
+  return data.map(value => {
+    if (value >= 20) {
+      return colorStatusBad;
+    } else if (value >= 10) {
+      return colorStatusFine;
+    } else {
+      return colorStatusGood;
     }
-
-    const colorStatusCheckBorder = (data) => {
-      return data.map(value => {
-        if (value >=20) {
-          return colorStatusBadBorder;
-        } else if (value >=10) {
-          return colorStatusFineBorder;
-        } else {
-          return colorStatusGoodBorder
-        }
-      })
-    }
-
-    const data = {
-      labels: errorTitles,
-      datasets: [
-        {
-          label: "Fouten",
-          data: errorAmounts,
-          borderColor: colorStatusCheckBorder(errorAmounts),
-          backgroundColor: colorStatusCheck(errorAmounts),
-          borderWidth: 3,
-        },
-      ],
-    };
-
-    const options = {
-      scales: {
-        y: {
-          beginAtZero: true,
-          title: {
-            display: false,
-            text: "Aantal",
-          },
-          grid: {
-            display: false,
-          },
-          ticks: {
-            font: {
-              family: fontFamily,
-              size: 16,
-            },
-          }
-        },
-        x: {
-          title: {
-            display: false,
-            text: "Type fouten",
-          },
-          grid: {
-            display: false,
-          },
-          ticks: {
-            font: {
-              family: fontFamily,
-              size: 16,
-            },
-          }
-        },
-      },
-      plugins: {
-        legend: {
-          display: false,
-        },
-        datalabels: {
-          color: colorBlack,
-          anchor: "center",
-          align: "center",
-          font: {
-            family: fontFamily,
-            weight: "bold",
-            size: 18,
-          },
-          formatter: (value) => value,
-        },
-        tooltip: {
-          enabled: true, // Enable tooltips
-          titleAlign: "center",
-          bodyAlign: "center",
-          backgroundColor: "#FFFFFF",
-          borderWidth: 2,
-          borderColor: colorBlue,
-          padding: 10,
-          displayColors: false,
-          titleFont: {
-            size: 18,
-            weight: "bold",
-            family: fontFamily,
-          },
-          titleColor: colorBlue,
-          bodyFont: {
-            size: 16,
-            weight: "normal",
-            family: fontFamily,
-          },
-          bodyColor: colorBlue,
-        },
-      },
-      responsive: true,
-      maintainAspectRatio: false,
-      onHover: (event, elements) => {
-        const canvas = event.native.target;
-        if (elements.length) {
-          canvas.style.cursor = "pointer";
-        } else {
-          canvas.style.cursor = "default";
-        }
-      },
-    };
-
-    const ctx = document.getElementById("bar-chart").getContext("2d");
-    chart = new Chart(ctx, {
-      type: "bar",
-      data: data,
-      options: options,
-    });
   });
+}
+
+const colorStatusCheckBorder = (data) => {
+  const colorStatusGoodBorder = rootStyles.getPropertyValue("--color-status-good-border");
+  const colorStatusFineBorder = rootStyles.getPropertyValue("--color-status-fine-border");
+  const colorStatusBadBorder = rootStyles.getPropertyValue("--color-status-bad-border");
+
+  return data.map(value => {
+    if (value >=20) {
+      return colorStatusBadBorder;
+    } else if (value >=10) {
+      return colorStatusFineBorder;
+    } else {
+      return colorStatusGoodBorder
+    }
+  })
+}
+
+
+  let chartLabels = errorTitles;
+  let chartData = errorAmounts;
+  let chartLabel = '# of Votes';
+  let chartBorderColor = colorStatusCheckBorder(errorAmounts);
+  let chartBackgroundColor = colorStatusCheck(errorAmounts);
+
+
 </script>
 
-<canvas id="bar-chart" aria-label="Soorten fouten grafiek">
-  <ul>
-    {#each results as item}
-      <li>{item.title}: {item.amount}</li>
-    {/each}
-  </ul>
-</canvas>
-
-<style>
-    canvas {
-    max-width: 100%;
-    height: 300px;
-  }
-</style>
+<Chart 
+  {chartLabels} 
+  {chartData} 
+  {chartLabel} 
+  {chartBorderColor}
+  {chartBackgroundColor}
+/>
