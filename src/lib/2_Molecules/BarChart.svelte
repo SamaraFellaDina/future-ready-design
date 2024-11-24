78ced492fa29fe67dfc23be19e8e8d30a88afe1a
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2024-11-24 23:01:02 +0100

    maken van gekleurde foutmeldingen
    tijdens het maken van #54 , was ik ook bezig met de stijling nadat ik #13 had gemaakt

--- src/lib/2_Molecules/BarChart.svelte
+++ src/lib/2_Molecules/BarChart.svelte
@@ -2,52 +2,33 @@
   import {Chart} from '../index.js'
   export let result = data.scans;
 
-  let results = result.result.slice(1);
 
+  let results = result.result.slice(1);
   let errorTitles = results.map((item) => item.title);
   let errorAmounts = results.map((item) => item.amount);
-
-  const rootStyles = getComputedStyle(document.documentElement);
-
-  const colorStatusCheck = (data) => {
-    const colorStatusGood = rootStyles.getPropertyValue("--color-status-good") || 'green';
-    const colorStatusFine = rootStyles.getPropertyValue("--color-status-fine") || 'yellow';
-    const colorStatusBad = rootStyles.getPropertyValue("--color-status-bad") || 'red';
-
-  return data.map(value => {
-    if (value >= 20) {
-      return colorStatusBad;
-    } else if (value >= 10) {
-      return colorStatusFine;
-    } else {
-      return colorStatusGood;
-    }
-  });
-}
-
-const colorStatusCheckBorder = (data) => {
-  const colorStatusGoodBorder = rootStyles.getPropertyValue("--color-status-good-border");
-  const colorStatusFineBorder = rootStyles.getPropertyValue("--color-status-fine-border");
-  const colorStatusBadBorder = rootStyles.getPropertyValue("--color-status-bad-border");
-
-  return data.map(value => {
-    if (value >=20) {
-      return colorStatusBadBorder;
-    } else if (value >=10) {
-      return colorStatusFineBorder;
-    } else {
-      return colorStatusGoodBorder
-    }
-  })
-}
-
 
   let chartLabels = errorTitles;
   let chartData = errorAmounts;
   let chartLabel = '# of Votes';
-  let chartBorderColor = colorStatusCheckBorder(errorAmounts);
-  let chartBackgroundColor = colorStatusCheck(errorAmounts);
+  let chartBorderColor = chartData.map(value => {
+    if (value >= 20) {
+      return 'red'; 
+    } else if (value >= 10) {
+      return 'orange'; 
+    } else {
+      return 'green'; 
+    }
+  });
 
+  let chartBackgroundColor = chartData.map(value => {
+    if (value >= 20) {
+      return '#ff00004a'; // Tomato red for high values
+    } else if (value >= 10) {
+      return '#ffa60053'; // Gold for medium values
+    } else {
+      return '#00800070'; // Lime green for low values
+    }
+  });
 
 </script>
 
