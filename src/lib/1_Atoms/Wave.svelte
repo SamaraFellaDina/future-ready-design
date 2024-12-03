3bafe527724e70d5a78e4bf2b5974626e99316ec
J3SS3HVA <jesse.leeuwerink@hva.nl>
2024-12-03 19:02:55 +0100

    1ste versie orbit animation
    volgende keer betere wave animation

---
+++ src/lib/1_Atoms/Wave.svelte
@@ -0,0 +1,45 @@
+<svg viewBox="0 0 290 30">
+  <path
+    class="wave"
+    fill="indigo"
+    stroke="indigo"
+    stroke-width="3"
+    stroke-linecap="round"
+    d="M0.100 25 c38 0 45-20 75-20s50 20 75 20 c38 0 45-5 75-20s50 20 75 20 c38 0 45-20 75-20s50 20 75 20 c38  0 45-20 75-20s50 20 75 20">
+  </path>
+</svg>
+
+<style>
+  svg {
+   
+    width: 200px;
+    
+  }
+
+  .wave {
+    animation: moveTheWave 3000ms linear infinite;
+    stroke-dasharray: 0 0 0 0; 
+    width: 200px;
+  }
+
+  /* @supports (animation-timeline: scroll(y)) {
+    .wave{
+        animation-timeline: scroll(y);
+        animation: moveTheWave linear;
+    }
+  } */
+
+  @keyframes moveTheWave {
+
+
+    0% {
+      stroke-dashoffset: 0;
+      transform: translate3d(0, 0, 0);
+    }
+
+    100% {
+      stroke-dashoffset: 1;
+      transform: translate3d(-278px, 0, 0);
+    }
+  }
+</style>
--- src/lib/2_Molecules/Orbit.svelte
+++ src/lib/2_Molecules/Orbit.svelte
@@ -1,14 +1,21 @@
+<script>
+  import { Wave } from '$lib';
+</script>
+
 <div class="spinner-box">
     <div class="big-orbit leo"> 
       <!-- <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Faf%2F52%2F70%2Faf52702337310d821f08dc8db63c9851.jpg&f=1&nofb=1&ipt=d1068ffb316b83af17defafe02e9cc301d30a5f08a5fa93da452d77189b3e95b&ipo=images" alt=""> -->
     </div>
     <div class="big-orbit2 leo2">
     </div>
-    <div class="planet leo"></div>
+    <div class="planet leo">
+      <Wave />
+      <div class="water-wave"></div>
+    </div>
   </div>
 
 <style>
-    .spinner-box {
+.spinner-box {
   height: 300px;
   display: flex;
   justify-content: center;
@@ -18,19 +25,18 @@
   right: 230px;
 }
 
-
 .big-orbit {
-	width: 165px;
-	height: 165px;
-  border: 1px solid var(--color-blue);
+	width: 155px;
+	height: 155px;
+  border: 3px solid var(--color-blue);
   animation: spin3D 3s linear .2s infinite;
   rotate: 200deg;
 }
 
 .big-orbit2 {
-	width: 155px;
-	height: 155px;
-  border: 1px solid var(--color-blue);
+	width: 145px;
+	height: 145px;
+  border: 3px solid var(--color-blue);
   animation: spin3D 3s linear .2s infinite;
   rotate: 200deg;
 }
@@ -40,14 +46,83 @@
   width: 100px;
   height: 100px;
   animation: unset;
-  z-index: -1;
+  overflow-y: hidden;
+  border: 2px solid indigo;
   
 }
+
+.planet::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    45deg,
+    var(--color-blue),
+    var(--color-lightblue),
+    var(--color-blue),
+    var(--color-lightblue),
+    var(--color-blue),
+    var(--color-lightblue),
+    var(--color-blue),
+    var(--color-lightblue)
+  );
+  background-size: 400%;
+  /*opacity: 0;*/
+  transition: 0.5s;
+  animation: animate 20s linear infinite;
+  z-index: -1;
+  border-radius: 50%;
+}
+
+.planet::after {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    45deg,
+    var(--color-blue),
+    var(--color-lightblue),
+    var(--color-blue),
+    var(--color-lightblue),
+    var(--color-blue),
+    var(--color-lightblue),
+    var(--color-blue),
+    var(--color-lightblue)
+  );
+  background-size: 400%;
+  /*opacity: 0;*/
+  filter: blur(20px);
+  transition: 0.5s;
+  animation: animate 20s linear infinite;
+  z-index: -1;
+  border-radius: 50%;
+}
+
+.water-wave{
+  background-color: indigo;
+  height: 50%;
+  width: 100%;
+  position: absolute;
+  top: 53%;
+  border-radius: 0 0 50% 50%;
+}
+
 
 @supports (animation-timeline: scroll(y)) {
  
   .big-orbit, .big-orbit2 {
     animation: spin3D linear;
+    animation-timeline: scroll(y); 
+  }
+
+  .planet::before, .planet::after{
+    animation: animate linear;
     animation-timeline: scroll(y); 
   }
 }
@@ -76,5 +151,21 @@
   to{
     transform: rotate3d(0deg);
   }
+}
+
+@keyframes animate
+{
+	0%
+	{
+		background-position: 0 0;
+	}
+	50%
+	{
+		background-position: 300% 0;
+	}
+	100%
+	{
+		background-position: 0 0;
+	}
 }
 </style>
