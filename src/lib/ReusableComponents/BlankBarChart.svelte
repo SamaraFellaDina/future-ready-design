63474d5bda3f11c8885f3c7f9cca1179388f956f
Khdulkadir <khdulkadir@gmail.com>
2025-01-06 22:29:37 +0100

    fixes #112

---
+++ src/lib/ReusableComponents/BlankBarChart.svelte
@@ -0,0 +1,159 @@
+<script>
+  import { Icon } from '$lib';
+  export let labels;
+  export let maxY;
+  export let stepY;
+</script>
+
+<figure>
+  <ul>
+    {#each Array.from({ length: maxY / stepY + 1 }, (_, i) => i * stepY) as y}
+      <li>{y}</li>
+    {/each}
+  </ul>
+
+  <ul>
+    {#each labels as { name, value }}
+      <li>
+        {#if name === "Contrast fouten"}
+          <Icon name="contrast-errors" />
+        {:else if name === "Structurele fouten"}
+          <Icon name="structural-errors" />
+        {:else if name === "Ontbrekende alt-teksten"}
+          <Icon name="missing-alt-texts" />
+        {:else if name === "Ontbrekende aria-labels"}
+          <Icon name="aria-label-errors" />
+        {/if}
+        <div>
+          <div>
+            <span>{name}</span>
+            <span>Aantal: {value}</span>
+          </div>
+        </div>
+        <div style="--final-height: {(value / maxY) * 100}%;"></div>
+        <span>{name}</span>
+      </li>
+    {/each}
+    </ul>
+</figure>
+
+<style>
+  @keyframes growHeight {
+    from {
+      height: 0;
+    }
+    to {
+      height: var(--final-height);
+    }
+  }
+
+  figure {
+    display: flex;
+    position: relative;
+    width: inherit;
+    height: 300px;
+    border-left: 2px solid var(--color-black);
+    border-bottom: 2px solid var(--color-black);
+    grid-column: 1 / -1;
+    margin-left: 30px;
+    margin-bottom: 40px;
+  }
+
+  ul:first-of-type {
+    position: absolute;
+    left: -35px;
+    display: flex;
+    flex-direction: column-reverse;
+    justify-content: space-between;
+    height: 100%;
+  }
+
+  ul:nth-of-type(2) {
+    position: relative;
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+  }
+
+  li {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: end;
+    align-items: center;
+  }
+
+  li > div:nth-of-type(2) {
+    border: 2px dashed var(--color-blue);
+    animation: growHeight 1s ease-out forwards;
+    height: 0;
+    --final-height: 100%;
+  }
+  
+  li > div:nth-of-type(1) {
+    width: 20px;
+    height: 20px;
+    background: var(--color-blue);
+    border-radius: 100%;
+    border: 4px solid var(--color-lightblue);
+    margin-top: 10px;
+    position: relative;
+    cursor: pointer;
+  }
+
+  li > span {
+    position: absolute;
+    bottom: -60px;
+    text-align: center;
+  }
+
+  li > div:nth-of-type(1) > div {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    position: absolute;
+    top: 20px;
+    background-color: var(--color-background-section);
+    padding: 10px;
+    border-radius: 10px;
+    border: 2px solid var(--color-blue);
+    white-space: nowrap;
+    justify-content: center;
+    align-items: center;
+    transform: translate(-50%, -50%);
+    opacity: 0;
+    pointer-events: none;
+    transition: 0.3s;
+  }
+
+  li > div:nth-of-type(1) > div span:first-of-type {
+    font-size: var(--font-size-medium);
+    font-weight: var(--font-weight-bold);
+    color: var(--color-blue);
+  }
+
+  li > div:nth-of-type(1) > div span:nth-of-type(2) {
+    font-size: var(--font-size-medium);
+    color: var(--color-blue);
+  }
+
+  li > div:nth-of-type(1):hover > div {
+    opacity: 1;
+    pointer-events: all;
+  }
+
+  @media screen and (max-width: 700px) {
+    li > span {
+      transform: rotate(45deg) scale(0.8);
+      bottom: -80px;
+    }
+
+    figure {
+      margin-bottom: 80px;
+    }
+
+    li > div:nth-of-type(1) > div {
+      white-space: unset;
+    }
+  }
+</style>
