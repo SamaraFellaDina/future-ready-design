099c6c09bf5f15f51c6bb83a5f01a7507a36f449
Khdulkadir <khdulkadir@gmail.com>
2025-01-09 23:12:39 +0100

    added components

--- src/lib/index.js
+++ src/lib/index.js
@@ -15,3 +15,9 @@ export { default as AutomaticScan } from "./Pages/Home/AutomaticScan.svelte";
 export { default as ErrorResult } from "./Pages/Home/ErrorResult.svelte";
 export { default as TypeGraph } from "./Pages/Home/TypeGraph.svelte";
 export { default as MonthlyNavigation } from "./Pages/Home/MonthlyNavigation.svelte";
+
+/*OVERZICHT*/
+export { default as AccessibillityScore } from "./Pages/Overzicht/AccessibillityScore.svelte";
+export { default as ScanResult } from "./Pages/Overzicht/ScanResult.svelte";
+export { default as SearchBar } from "./Pages/Overzicht/SearchBar.svelte";
+export { default as SearchResults } from "./Pages/Overzicht/SearchResults.svelte";
