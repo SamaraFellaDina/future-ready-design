820a1131af4ada4d4eadf125228a9b3b141830ca
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-01-10 11:41:30 +0100

    pagina omzetten naar een component
    Ik had de opmaak en structuur in de pagina zelf gemaakt, maar nu heb ik het omgezet naar een component, om het zo meer te refactoren

--- src/lib/Pages/Home/PageNavigation.svelte
+++ src/lib/Pages/Home/PageNavigation.svelte
@@ -1,30 +1,61 @@
-
+<script>
+  import { Icon, BlankDonutChart} from '$lib';
+  export let data = data;
+  console.log(data)
+</script>
 
 <ul>
+  {#each data.sites as {url, title}}
   <li>
-    <svg>
-
-    </svg>
-    
+    <Icon name='home-icon' width=20 height=20/>
     <section>
-      <h2>Nieuwekijk</h2>
-      <p>www.nieuwekijk.nl</p>
+      <h2>{title}</h2>
+      <p>{url.replace(/^https?:\/\//, "")}</p>
       <div>
         <svg></svg>
         <p>toegankelijk</p>
       </div>
     </section>
-
-    <canvas>
-
-    </canvas>
+      <BlankDonutChart percentage=80/>
   </li>
+  {/each}
 </ul>
 
 <style>
-  li {
+
+ul {
+    --average-gap: .8em;
+    display: block;
+    list-style-type: none;
+    margin-block-start: 0;
+    margin-block-end: 0;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    padding-inline-start: 0;
+    unicode-bidi: isolate;
+
+    display: grid;
+    gap: var(--average-gap);
+
+  }
+
+  ul li {
     background-color: var(--color-background-section);
     border-radius: var(--section-border-radius);
     padding: var(--average-padding);
+
+    display: flex;
+    gap: var(--average-gap);
   }
+
+  ul li svg {
+    --icon-ratio: 1.2em;
+    width: var(--icon-ratio);
+    height: var(--icon-ratio);
+  }
+  @media (min-width: 700px) {
+      ul {
+        grid-template-columns: 50% 50%;
+      }
+    }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,81 +1,19 @@
 <script>
-  import { Icon, BlankDonutChart} from '$lib';
+  import { PageNavigation} from '$lib';
   export let data = data;
-  let sites = data.sites
-  console.log(sites)
-
 </script>
 
 <main>
-  <ul>
-    {#each sites as {url, title}}
-    <li>
-      <Icon name='home-icon' width=20 height=20/>
-      <section>
-        <h2>{title}</h2>
-        <p>{url.replace(/^https?:\/\//, "")}</p>
-        <div>
-          <svg></svg>
-          <p>toegankelijk</p>
-        </div>
-      </section>
-        <BlankDonutChart percentage=80/>
-    </li>
-    {/each}
-  </ul>
+  <PageNavigation {data}/>
 </main>
 
 <style>
 
   main {
-    display: grid;
-    grid-template-columns: 2.2fr 0.8fr; 
-    grid-template-rows: 0.2fr 0.8fr 1fr 1.2fr;   
-    gap: 20px;  
     margin: 0 auto;
     padding: 30px 10px;
     max-width: 1100px;
   }
-
-  ul {
-    --average-gap: .8em;
-    display: block;
-    list-style-type: none;
-    margin-block-start: 0;
-    margin-block-end: 0;
-    margin-inline-start: 0px;
-    margin-inline-end: 0px;
-    padding-inline-start: 0;
-    unicode-bidi: isolate;
-
-    display: grid;
-    gap: var(--average-gap);
-
-  }
-
-  li {
-    background-color: var(--color-background-section);
-    border-radius: var(--section-border-radius);
-    padding: var(--average-padding);
-
-    display: flex;
-    gap: var(--average-gap);
-  }
-
-  li svg {
-    --icon-ratio: 1.2em;
-    width: var(--icon-ratio);
-    height: var(--icon-ratio);
-  }
-
-
-  @media (min-width: 700px) {
-
-      ul {
-        grid-template-columns: 50% 50%;
-      }
-    }
-
 
   @media only screen and (max-width: 1600px) {
     main {
@@ -89,7 +27,4 @@
       padding-left: 70px;
     }
   }
-
-  
-
 </style>
