23342c8b9f83750a1a1928787e40eea0e9cd9ae6
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-01-10 17:54:12 +0100

    revert TypeGraph.svelte

--- src/lib/Pages/Home/TypeGraph.svelte
+++ src/lib/Pages/Home/TypeGraph.svelte
@@ -1,22 +1,30 @@
 <script>
   import { BlankBarChart } from '$lib';
   export let result = data.scans;
-  let results = result.result.slice(1);
-  let labels = results.map((item) => ({
+
+  // Reactieve berekeningen
+  $: results = result.result.slice(1);
+  $: labels = results.map((item) => ({
     name: item.title,
     value: item.amount
   }));
-  let maxY = Math.max(...results.map((item) => item.amount)) + 10;
-  let stepY;
-  if (maxY <= 20) {
-    stepY = 5;
-  } else if (maxY > 20 && maxY <= 50) {
-    stepY = 10;
-  } else if (maxY > 50 && maxY <= 100) {
-    stepY = 20;
-  } else if (maxY > 100) {
-    stepY = 50;
-  }
+  $: maxY = Math.max(...results.map((item) => item.amount)) + 10;
+  $: stepY = maxY <= 20 
+    ? 5 : maxY <= 50 
+      ? 10 : maxY <= 100 
+        ? 20 : 50;
+
+  // let maxY = Math.max(...results.map((item) => item.amount)) + 10;
+  // let stepY;
+  // if (maxY <= 20) {
+  //   stepY = 5;
+  // } else if (maxY > 20 && maxY <= 50) {
+  //   stepY = 10;
+  // } else if (maxY > 50 && maxY <= 100) {
+  //   stepY = 20;
+  // } else if (maxY > 100) {
+  //   stepY = 50;
+  // }
 </script>
 
 <section>
@@ -25,7 +33,6 @@
 </section>
 
 <style>
-  
   section {
     background-color: var(--color-background-section);
     border-radius: var(--section-border-radius);
