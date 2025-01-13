2043db3f1e401846d564466cf69602f56a1817a0
Khdulkadir <khdulkadir@gmail.com>
2025-01-13 14:42:26 +0100

    progress veranderd naar meter

--- src/lib/ReusableComponents/BlankDonutChart.svelte
+++ src/lib/ReusableComponents/BlankDonutChart.svelte
@@ -65,78 +65,80 @@
 
 <script>
   export let percentage;
-  $: donutColor = percentage < 50 ? 'var(--color-status-bad-border)' : percentage < 80 ? 'var(--color-status-fine-border)' : percentage < 99 ? 'var(--color-status-decent-border)' : 'var(--color-status-good-border)';
+
+  $: donutColor = percentage < 50 
+    ? 'var(--color-status-bad-border)' 
+    : percentage < 80 
+    ? 'var(--color-status-fine-border)' 
+    : percentage < 99 
+    ? 'var(--color-status-decent-border)' 
+    : 'var(--color-status-good-border)';
 </script>
 
 <div>
-  <progress max="100" value={percentage} style="--donutcolor: {donutColor}">{percentage}</progress>
+  <meter 
+    min="0" 
+    max="100" 
+    value={percentage} 
+    style="--donutcolor: {donutColor}">
+    {percentage}%
+  </meter>
 </div>
 
 <style>
-    @property --x {
+  @property --x {
     syntax: '<integer>';
     inherits: true;
-    initial-value: 100; 
+    initial-value: 100;
   }
 
   @property --y {
     syntax: '<number>';
     inherits: true;
-    initial-value: 1; 
+    initial-value: 1;
   }
 
-  progress {
+  meter {
     width: 135px;
     height: 135px;
     appearance: none;
     position: relative;
-    animation: y 2s .5s both;
+    animation: y 2s 0.5s both;
     timeline-scope: --progress;
   }
-  progress::-webkit-progress-bar {
-    overflow: auto;
-    visibility: hidden;
+
+  meter::-webkit-meter-bar {
+    background-color: #ccc;
+    border-radius: 50%;
+    overflow: hidden;
+    position: relative;
   }
 
-  progress::-webkit-progress-value {
-    view-timeline: --progress inline;
+  meter::-webkit-meter-optimum-value {
+    background-color: var(--donutcolor);
+    border-radius: 50%;
+    mask: 
+      conic-gradient(#0000 0 0) subtract content-box,
+      conic-gradient(#000 calc(1% * var(--y) * var(--x)), #0000 0);
   }
 
-  progress::before,
-  progress::after {
+  meter::before {
+    content: counter(val) "%";
+    counter-reset: val calc(var(--y) * var(--x));
     position: absolute;
     inset: 0;
-    border-radius: 50%;
-    animation: x linear;
-    animation-timeline: --progress;
-    animation-range: entry 100% exit 100%;
-  }
-
-  progress::before {
-    content: counter(val) "%";
-    counter-reset: val calc(var(--y)*var(--x));
     display: grid;
     place-content: center;
     font-size: var(--font-size-large);
     font-weight: var(--font-weight-bold);
-    border: 15.5px solid #ccc;
-  }
-
-  progress::after {
-    content: "";
-    background-color: var(--donutcolor);
-    padding: 15px;
-    border-radius: 50%;
-    mask: 
-      conic-gradient(#0000 0 0) subtract content-box,
-      conic-gradient(#000 calc(1%*var(--y)*var(--x)),#0000 0);
+    color: #000;
   }
 
   @keyframes x {
-    to {--x: 0}
+    to { --x: 0; }
   }
 
   @keyframes y {
-    0% {--y: 0}
+    0% { --y: 0; }
   }
 </style>
