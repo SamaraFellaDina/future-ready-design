6065c32207121afb7fd25353bad5d9f6068a5dc8
Khdulkadir <khdulkadir@gmail.com>
2025-01-23 19:59:04 +0100

    fixes #224 view transition toegevoegd

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,19 @@
 <script>
   import { Header, Footer } from '$lib';
+  import { onNavigate } from '$app/navigation'
   export let data;
   export let company = data.sites;
 
+  onNavigate((navigation) => {
+    if (!document.startViewTransition) return
+
+    return new Promise((resolve) => {
+      document.startViewTransition(async () => {
+        resolve()
+        await navigation.complete
+      })
+    })
+  })
 </script>
 
 <Header {company}/>
